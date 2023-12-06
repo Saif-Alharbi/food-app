bd21af00e6850e0bd0a519ecc84e8a693fe4524f
Saif Alharbi <98286934+Saif-Alharbi@users.noreply.github.com>
2023-12-06 20:44:40 +0300

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,8 +4,7 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer Jw0oIMgpId1HV8x-mogAapr36SVRDSAM00qOEvAmLyxCaOV1I0T6kzJbSvazjA6Q7sNS46uHfHzRzLLAESkHYv3ES50h-sUQwtwvh836OsN-D5UwO6ObMswyxDM6YXYx',
+      'Bearer ',
   },
 });
 
-// Jw0oIMgpId1HV8x-mogAapr36SVRDSAM00qOEvAmLyxCaOV1I0T6kzJbSvazjA6Q7sNS46uHfHzRzLLAESkHYv3ES50h-sUQwtwvh836OsN-D5UwO6ObMswyxDM6YXYx
